3d031cb3c455023be7bc6af21c90b869acec8094
kushkurl <kushagra.kurl@gmail.com>
2022-02-20 02:33:22 -0500

    Add project files.

---
+++ CalculatorWebAPI/Controllers/MathController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorWebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        public double Add(int lVal, int rVal)
+        {
+            return lVal + rVal;
+        }
+        [HttpGet]
+        public double Subtract(int lVal, int rVal)
+        {
+            return lVal - rVal;
+        }
+        [HttpGet]
+        public double Multiply(int lVal, int rVal)
+        {
+            return lVal * rVal;
+        }
+        [HttpGet]
+        public double Divide(int lVal, int rVal)
+        {
+            return lVal/rVal;
+        }
+    }
+}
