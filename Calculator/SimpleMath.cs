7a2cc26bdba66986e47846cf3591b1a77651e66a
kushkurl <kushagra.kurl@gmail.com>
2022-02-21 12:42:53 -0500

    added clss library for calculations and using it instead

---
+++ Calculator/SimpleMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class SimpleMath
+    {
+        public double Calculate(int num1, int num2, string operation)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case "Add":
+                    result = num1 + num2;
+                    break;
+                case "Sub":
+                    result = num1 + num2;
+                    break;
+                case "Mult":
+                    result = num1 * num2;
+                    break;
+                case "Div":
+                    result = num1 / num2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
--- CalculatorWebAPI/Controllers/MathController.cs
+++ CalculatorWebAPI/Controllers/MathController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Calculator;
 
 namespace CalculatorWebAPI.Controllers
 {
@@ -7,29 +8,30 @@ namespace CalculatorWebAPI.Controllers
     [ApiController]
     public class MathController : ControllerBase
     {
+        SimpleMath math = new SimpleMath();
         [HttpGet]
         public double Add(int lVal, int rVal)
         {
             Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:44362");
-            return lVal + rVal;
+            return math.Calculate(lVal, rVal, "Add");
         }
         [HttpGet]
         public double Subtract(int lVal, int rVal)
         {
             Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:44362");
-            return lVal - rVal;
+            return math.Calculate(lVal, rVal, "Sub");
         }
         [HttpGet]
         public double Multiply(int lVal, int rVal)
         {
             Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:44362");
-            return lVal * rVal;
+            return math.Calculate(lVal, rVal, "Mult");
         }
         [HttpGet]
         public double Divide(int lVal, int rVal)
         {
             Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:44362");
-            return lVal/rVal;
+            return math.Calculate(lVal, rVal, "Div");
         }
     }
 }
